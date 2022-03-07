c0493214784aaf1c6d07d3b2c64d0189fee276a7
Mustafa Mousa <mustafaomousa@icloud.com>
2022-03-06 18:35:56 -0600

    initialized login manager, user_loader cb, init and migrate db with app

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,26 @@
 import os
 from flask import Flask, render_template, request, session, redirect
 from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 
+from .models import db, User
 from .config import Config
 
 app = Flask(__name__)
 
+login = LoginManager(app)
+login.login_view = 'auth.unauthorized'
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 app.config.from_object(Config)
+
+db.init_app(app)
+Migrate(app, db)
 
 CORS(app)
 
